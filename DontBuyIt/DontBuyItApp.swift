5d392c3e1518e886e143eda103ec57e2e5945f6b
pavelddi <pavel.borisov.ddidev@gmail.com>
2022-05-22 22:17:32 +0300

    Initial commit.

---
+++ DontBuyIt/DontBuyItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontBuyItApp.swift
+//  DontBuyIt
+//
+//  Created by Pavel Borisov on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DontBuyItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
