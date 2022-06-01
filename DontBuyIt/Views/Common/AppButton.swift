76c9bd274e13d54ff6e4c4232eb6da00e4bdcdcb
pavelddi <pavel.borisov.ddidev@gmail.com>
2022-06-01 11:13:37 +0300

    Imoproved AppButton UI a bit.

--- DontBuyIt/Views/Common/AppButton.swift
+++ DontBuyIt/Views/Common/AppButton.swift
@@ -25,6 +25,22 @@ struct AppButton: View {
         isActive ? activeColor.opacity(0.3) : .black.opacity(0.3)
     }
     
+    init(isActive: Binding<Bool>,
+         showDot: Bool = false,
+         activeColor: Color,
+         text: String,
+         verticalPadding: CGFloat = 12,
+         horizontalPadding: CGFloat = .zero,
+         action: @escaping (() -> Void)) {
+        _isActive = isActive
+        self.showDot = showDot
+        self.activeColor = activeColor
+        self.text = text
+        self.verticalPadding = verticalPadding
+        self.horizontalPadding = horizontalPadding
+        self.action = action
+    }
+    
     var body: some View {
         Button(action: action, label: {
             HStack {
@@ -44,28 +60,14 @@ struct AppButton: View {
         })
         .background(backgroundColor)
         .overlay(
-            RoundedRectangle(cornerRadius: 20)
+            Capsule()
                 .stroke(isActive ? activeColor : backgroundColor,
                         lineWidth: 3)
         )
-        .cornerRadius(20)
+        .clipShape(
+            Capsule()
+        )
         .shadow(color: shadowColor, radius: 3)
-    }
-    
-    init(isActive: Binding<Bool>,
-         showDot: Bool = false,
-         activeColor: Color,
-         text: String,
-         verticalPadding: CGFloat = 12,
-         horizontalPadding: CGFloat = .zero,
-         action: @escaping (() -> Void)) {
-        _isActive = isActive
-        self.showDot = showDot
-        self.activeColor = activeColor
-        self.text = text
-        self.verticalPadding = verticalPadding
-        self.horizontalPadding = horizontalPadding
-        self.action = action
     }
 }
 
